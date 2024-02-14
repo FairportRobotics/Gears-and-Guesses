9426d277af7474a12a45d79bcb9a0d66e1f62b59
IMightExist <90283058+IMightExist7@users.noreply.github.com>
2024-02-13 19:16:18 -0500

    randNum

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+import random as rand
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("test.html")
+    a = rand.randint(0, 10000)
+    return render_template("test.html", a = a)
 
 app.run(host="0.0.0.0", port=80)
