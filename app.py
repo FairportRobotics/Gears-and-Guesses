2b19c53887b80c22a71f35fc33cc488acf498fa3
IMightExist <90283058+IMightExist7@users.noreply.github.com>
2024-02-13 19:45:50 -0500

    blue alliance match data

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 import random as rand
+import json
+import requests
+import os
 
 app = Flask(__name__)
+
+tba_api_key = os.environ.get("TBA_API_KEY")
+
 
 @app.route("/")
 def hello_world():
@@ -9,3 +15,12 @@ def hello_world():
     return render_template("test.html", a = a)
 
 app.run(host="0.0.0.0", port=80)
+
+def tba_matches(key: str):
+    headers = { "X-TBA-Auth-Key": tba_api_key }
+    response = requests.get(f"https://www.thebluealliance.com/api/v3/event/{key}/matches", headers)
+    with open(f'matches_{key}.json', 'wb') as f:
+        f.write(response.content)
+    return()
+
+tba_matches("2023nyrr")
