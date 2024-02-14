2ba962acf9a798c6e8b6fd10f7802c0cb37cc300
IMightExist <90283058+IMightExist7@users.noreply.github.com>
2024-02-13 19:08:06 -0500

    test

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<html><body><p>Hello, World!</p></body></html>"
+    return render_template("test.html")
 
 app.run(host="0.0.0.0", port=80)
